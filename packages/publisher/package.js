4cf5166eeaa10dcaceb5d5cfcfe14f7cdd637666
Daniel Beilinson <beilinson@gmail.com>
2017-09-06 00:42:12 +0300

    Update translations.

--- packages/publisher/package.js
+++ packages/publisher/package.js
@@ -63,5 +63,30 @@ export default {
         commandGroups: ['utils']
       }
     ])
+
+    config.addLabel('find-and-replace-title', {
+      en: 'Find and Replace',
+      ru: 'Поиск и замена'
+    })
+    config.addLabel('person-name-placeholder', {
+      en: 'Enter person\'s name',
+      ru: 'Введите имя персоналии'
+    })
+    config.addLabel('person-description-placeholder', {
+      en: 'Enter person\'s description',
+      ru: 'Введите описание персоналии'
+    })
+    config.addLabel('person-global-placeholder', {
+      en: 'Global person',
+      ru: 'Глобальная персоналия'
+    })
+    config.addLabel('сommentary-name-placeholder', {
+      en: 'Enter comment title',
+      ru: 'Заголовок комментария'
+    })
+    config.addLabel('сommentary-description-placeholder', {
+      en: 'Enter comment',
+      ru: 'Введите комментарий'
+    })
   }
 }
